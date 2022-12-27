890685f90018b7ae1598598d386ca2eb34b38ed2
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2022-12-26 23:49:27 -0300

    redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,54 +2,13 @@ import Header from 'components/Header'
 import styles from './Home.module.scss'
 import wristwatch from 'assets/inicial.png'
 
-import automotivoThumb from 'assets/categories/thumbnail/automotivo.png';
-import eletronicosThumb from 'assets/categories/thumbnail/eletronicos.png';
-import escritorioThumb from 'assets/categories/thumbnail/escritorio.png';
-import jogosThumb from 'assets/categories/thumbnail/jogos.png';
-import somThumb from 'assets/categories/thumbnail/som.png';
-import automotivoHeader from 'assets/categories/header/automotivo.png';
-import eletronicosHeader from 'assets/categories/header/eletronicos.png';
-import escritorioHeader from 'assets/categories/header/escritorio.png';
-import jogosHeader from 'assets/categories/header/jogos.png';
-import somHeader from 'assets/categories/header/som.png';
-
 import { useNavigate } from 'react-router-dom';
-
-const categories = [{
-    nome: 'Eletrônicos',
-    thumbnail: eletronicosThumb,
-    header: eletronicosHeader,
-    id: 'eletronicos',
-    descricao: 'Os melhores e mais atuais dispositivos eletrônicos estão aqui!'
-}, {
-    nome: 'Automotivo',
-    thumbnail: automotivoThumb,
-    header: automotivoHeader,
-    id: 'automotivos',
-    descricao: 'Encontre aqui equipamentos para deixar seu carro com a sua cara!'
-}, {
-    nome: 'Jogos',
-    thumbnail: jogosThumb,
-    header: jogosHeader,
-    id: 'jogos',
-    descricao: 'Adquira os consoles e jogos mais atuais do mercado !'
-}, {
-    nome: 'Escritório',
-    thumbnail: escritorioThumb,
-    header: escritorioHeader,
-    id: 'escritorio',
-    descricao: 'Tudo para o que escritório ficar incrível!'
-}, {
-    nome: 'Som e imagem',
-    thumbnail: somThumb,
-    header: somHeader,
-    id: 'som',
-    descricao: 'Curta suas músicas e seus filmes com a melhor qualidade!'
-}]
+import { useSelector } from 'react-redux';
 
 export default function Home() {
 
     const navigate = useNavigate();
+    const categories = useSelector(state => state.categories)
 
     return (
         <div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import categoriesSlice from './reducers/categories.js'
 
 const store = configureStore({
-
-
+    reducer: {
+        categories: categoriesSlice
+    }
 });
+
+export default store;
